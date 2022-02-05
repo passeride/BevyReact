8e8b7bffdaca9a6866eff7ee3e8c7ca691435d8f
Lukas <lukas@passeride.com>
2022-02-05 21:25:04 +0100

    Working on smart shit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use bevy::prelude::*;
+use bevy::{ecs::world, prelude::*};
 use bevy_prototype_lyon::prelude::*;
+use rand::Rng;
 use std::cell::RefCell;
+use uuid::Uuid;
 use wasm_bindgen::prelude::*;
 
 thread_local!(static GLOBAL_MOVE_UP: RefCell<bool> = RefCell::new(false));
@@ -28,22 +30,63 @@ pub fn move_left() {
     GLOBAL_MOVE_LEFT.with(|text| *text.borrow_mut() = true);
 }
 
+fn get_random_buf() -> Result<[u8; 32], getrandom::Error> {
+    let mut buf = [0u8; 32];
+    getrandom::getrandom(&mut buf)?;
+    Ok(buf)
+}
+
+#[derive(Component)]
+struct Destination {
+    id: Uuid,
+}
+
+#[derive(Component)]
+struct TravelPath {
+    pub path: Vec<Uuid>,
+    pub path_count: usize,
+}
+
+fn get_random_f32() -> f32 {
+    return rand::thread_rng().gen_range(-200.0..200.0);
+}
+
+fn get_random_uuid() -> uuid::Uuid {
+    uuid::Uuid::new_v4()
+}
+
 fn main() {
     App::new()
         .insert_resource(WindowDescriptor {
-            title: "I am a window!".to_string(),
-            #[cfg(target_arch = "wasm32")]
-            canvas: Some(String::from(".game")),
             width: 500.,
             height: 300.,
+            title: "I am a window!".to_string(),
             vsync: true,
+            #[cfg(target_arch = "wasm32")]
+            canvas: Some(String::from(".game")),
             ..Default::default()
         })
         .insert_resource(Msaa { samples: 4 })
+        .insert_resource(TravelPath {
+            path: vec![],
+            path_count: 2,
+        })
         .add_plugins(DefaultPlugins)
         .add_plugin(ShapePlugin)
         .add_startup_system(setup)
+        .add_system(update_path)
         .run();
+}
+
+fn update_path(query: Query<(&Destination, &Transform)>, mut path: Option<ResMut<TravelPath>>) {
+    let path_unwrap = path.unwrap();
+    // info!("Adding another dest to path");
+    if (path_unwrap.path.len() < path_unwrap.path_count) {
+        info!("Adding another dest to path");
+        for (dest, trans) in query.iter() {
+            info!("Here is {}", dest.id);
+        }
+    }
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -55,15 +98,21 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     };
 
     for x in 0..10 {
-        commands.spawn_bundle(GeometryBuilder::build_as(
+        let rnd_x = get_random_f32();
+        let rnd_y = get_random_f32();
+        let mut entity = commands.spawn_bundle(GeometryBuilder::build_as(
             &shape,
             DrawMode::Outlined {
                 fill_mode: FillMode::color(Color::CYAN),
                 outline_mode: StrokeMode::new(Color::BLACK, 10.0),
             },
-            Transform::from_translation(Vec3::new(x as f32, x as f32, 0.0)),
+            Transform::from_translation(Vec3::new(rnd_x, rnd_y, 0.0)),
         ));
+        entity.insert(Destination {
+            id: get_random_uuid(),
+        });
     }
+
     commands.spawn_bundle(SpriteBundle {
         texture: asset_server.load("icon.png"),
         ..Default::default()
