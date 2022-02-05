02aad1eb0bf4d27153551661645c3ad9a1a03302
Lukas <lukas@passeride.com>
2022-02-05 13:01:42 +0100

    Adding shapes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
 use std::cell::RefCell;
 use wasm_bindgen::prelude::*;
 
@@ -38,263 +39,33 @@ fn main() {
             vsync: true,
             ..Default::default()
         })
+        .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
+        .add_plugin(ShapePlugin)
         .add_startup_system(setup)
-        .add_system(movement)
-        .add_system(animate_light_direction)
         .run();
 }
 
-#[derive(Component)]
-struct Movable;
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    let shape = shapes::RegularPolygon {
+        sides: 6,
+        feature: shapes::RegularPolygonFeature::Radius(20.0),
+        ..shapes::RegularPolygon::default()
+    };
 
-/// set up a simple 3D scene
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // ground plane
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 10.0 })),
-        material: materials.add(StandardMaterial {
-            base_color: Color::WHITE,
-            perceptual_roughness: 1.0,
-            ..Default::default()
-        }),
-        ..Default::default()
-    });
-
-    // left wall
-    let mut transform = Transform::from_xyz(2.5, 2.5, 0.0);
-    transform.rotate(Quat::from_rotation_z(std::f32::consts::FRAC_PI_2));
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Box::new(5.0, 0.15, 5.0))),
-        transform,
-        material: materials.add(StandardMaterial {
-            base_color: Color::INDIGO,
-            perceptual_roughness: 1.0,
-            ..Default::default()
-        }),
-        ..Default::default()
-    });
-    // back (right) wall
-    let mut transform = Transform::from_xyz(0.0, 2.5, -2.5);
-    transform.rotate(Quat::from_rotation_x(std::f32::consts::FRAC_PI_2));
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Box::new(5.0, 0.15, 5.0))),
-        transform,
-        material: materials.add(StandardMaterial {
-            base_color: Color::INDIGO,
-            perceptual_roughness: 1.0,
-            ..Default::default()
-        }),
-        ..Default::default()
-    });
-
-    // cube
-    commands
-        .spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-            material: materials.add(StandardMaterial {
-                base_color: Color::PINK,
-                ..Default::default()
-            }),
-            transform: Transform::from_xyz(0.0, 0.5, 0.0),
-            ..Default::default()
-        })
-        .insert(Movable);
-    // sphere
-    commands
-        .spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::UVSphere {
-                radius: 0.5,
-                ..Default::default()
-            })),
-            material: materials.add(StandardMaterial {
-                base_color: Color::LIME_GREEN,
-                ..Default::default()
-            }),
-            transform: Transform::from_xyz(1.5, 1.0, 1.5),
-            ..Default::default()
-        })
-        .insert(Movable);
-
-    // ambient light
-    commands.insert_resource(AmbientLight {
-        color: Color::ORANGE_RED,
-        brightness: 0.02,
-    });
-
-    // red point light
-    commands
-        .spawn_bundle(PointLightBundle {
-            // transform: Transform::from_xyz(5.0, 8.0, 2.0),
-            transform: Transform::from_xyz(1.0, 2.0, 0.0),
-            point_light: PointLight {
-                intensity: 1600.0, // lumens - roughly a 100W non-halogen incandescent bulb
-                color: Color::RED,
-                shadows_enabled: true,
-                ..Default::default()
+    for x in 0..10 {
+        commands.spawn_bundle(GeometryBuilder::build_as(
+            &shape,
+            DrawMode::Outlined {
+                fill_mode: FillMode::color(Color::CYAN),
+                outline_mode: StrokeMode::new(Color::BLACK, 10.0),
             },
-            ..Default::default()
-        })
-        .with_children(|builder| {
-            builder.spawn_bundle(PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::UVSphere {
-                    radius: 0.1,
-                    ..Default::default()
-                })),
-                material: materials.add(StandardMaterial {
-                    base_color: Color::RED,
-                    emissive: Color::rgba_linear(100.0, 0.0, 0.0, 0.0),
-                    ..Default::default()
-                }),
-                ..Default::default()
-            });
-        });
-
-    // green point light
-    commands
-        .spawn_bundle(PointLightBundle {
-            // transform: Transform::from_xyz(5.0, 8.0, 2.0),
-            transform: Transform::from_xyz(-1.0, 2.0, 0.0),
-            point_light: PointLight {
-                intensity: 1600.0, // lumens - roughly a 100W non-halogen incandescent bulb
-                color: Color::GREEN,
-                shadows_enabled: true,
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .with_children(|builder| {
-            builder.spawn_bundle(PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::UVSphere {
-                    radius: 0.1,
-                    ..Default::default()
-                })),
-                material: materials.add(StandardMaterial {
-                    base_color: Color::GREEN,
-                    emissive: Color::rgba_linear(0.0, 100.0, 0.0, 0.0),
-                    ..Default::default()
-                }),
-                ..Default::default()
-            });
-        });
-
-    // blue point light
-    commands
-        .spawn_bundle(PointLightBundle {
-            // transform: Transform::from_xyz(5.0, 8.0, 2.0),
-            transform: Transform::from_xyz(0.0, 4.0, 0.0),
-            point_light: PointLight {
-                intensity: 1600.0, // lumens - roughly a 100W non-halogen incandescent bulb
-                color: Color::BLUE,
-                shadows_enabled: true,
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .with_children(|builder| {
-            builder.spawn_bundle(PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::UVSphere {
-                    radius: 0.1,
-                    ..Default::default()
-                })),
-                material: materials.add(StandardMaterial {
-                    base_color: Color::BLUE,
-                    emissive: Color::rgba_linear(0.0, 0.0, 100.0, 0.0),
-                    ..Default::default()
-                }),
-                ..Default::default()
-            });
-        });
-
-    // directional 'sun' light
-    const HALF_SIZE: f32 = 10.0;
-    commands.spawn_bundle(DirectionalLightBundle {
-        directional_light: DirectionalLight {
-            // Configure the projection to better fit the scene
-            shadow_projection: OrthographicProjection {
-                left: -HALF_SIZE,
-                right: HALF_SIZE,
-                bottom: -HALF_SIZE,
-                top: HALF_SIZE,
-                near: -10.0 * HALF_SIZE,
-                far: 10.0 * HALF_SIZE,
-                ..Default::default()
-            },
-            shadows_enabled: true,
-            ..Default::default()
-        },
-        transform: Transform {
-            translation: Vec3::new(0.0, 2.0, 0.0),
-            rotation: Quat::from_rotation_x(-std::f32::consts::FRAC_PI_4),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
-
-    // camera
-    commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..Default::default()
-    });
-}
-
-fn animate_light_direction(
-    time: Res<Time>,
-    mut query: Query<&mut Transform, With<DirectionalLight>>,
-) {
-    for mut transform in query.iter_mut() {
-        transform.rotate(Quat::from_rotation_y(time.delta_seconds() * 0.5));
+            Transform::from_translation(Vec3::new(x as f32, x as f32, 0.0)),
+        ));
     }
-}
-
-fn movement(
-    input: Res<Input<KeyCode>>,
-    time: Res<Time>,
-    mut query: Query<&mut Transform, With<Movable>>,
-) {
-    for mut transform in query.iter_mut() {
-        let mut direction = Vec3::ZERO;
-        GLOBAL_MOVE_LEFT.with(|text| {
-            if *text.borrow() {
-                direction.x -= 1.0;
-                *text.borrow_mut() = false;
-            }
-        });
-        GLOBAL_MOVE_RIGHT.with(|text| {
-            if *text.borrow() {
-                direction.x += 1.0;
-                *text.borrow_mut() = false;
-            }
-        });
-        GLOBAL_MOVE_UP.with(|text| {
-            if *text.borrow() {
-                direction.y += 1.0;
-                *text.borrow_mut() = false;
-            }
-        });
-        GLOBAL_MOVE_DOWN.with(|text| {
-            if *text.borrow() {
-                direction.y -= 1.0;
-                *text.borrow_mut() = false;
-            }
-        });
-        if input.pressed(KeyCode::Up) {
-            direction.y += 1.0;
-        }
-        if input.pressed(KeyCode::Down) {
-            direction.y -= 1.0;
-        }
-        if input.pressed(KeyCode::Left) {
-            direction.x -= 1.0;
-        }
-        if input.pressed(KeyCode::Right) {
-            direction.x += 1.0;
-        }
-
-        transform.translation += time.delta_seconds() * 2.0 * direction;
-    }
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("icon.png"),
+        ..Default::default()
+    });
 }
